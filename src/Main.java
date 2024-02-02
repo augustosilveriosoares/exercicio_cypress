2fb0f3da2ccab2a87574d824361d5b8a558a45f0
Augusto Silvério Soares <falecomsilverio@gmail.com>
2024-02-02 14:08:29 -0300

    Exericio Mod19

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
-
+import java.lang.annotation.Annotation;
 
 public class Main {
 
     public static void main (String args[]){
+
+        Objeto obj = new Objeto();
+
+        if(obj.getClass().isAnnotationPresent(AnnotationTable.class)){
+            AnnotationTable ano = obj.getClass().getAnnotation(AnnotationTable.class);
+            System.out.println(ano.getClass().getName());
+            System.out.println(ano.tableName());
+        }
+
 
 
     }
